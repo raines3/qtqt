f5dbc658cc9ee6747cded75963f6fbabe305c06d
rainesCode <markxxxzzz98@gmail.com>
2026-06-05 18:38:36 +0900

    Добавлена функция

--- 2.py
+++ 2.py
@@ -10,17 +10,24 @@ class ParcentApp(QWidget):
 
         self.num_input = QLineEdit(self)
         self.num_input.setPlaceholderText("Число")
+        self.num_input2 = QLineEdit(self)
         self.btn = QPushButton(self)
         self.label = QLabel(self)
+
+        self.btn.clicked.connect(self.calculate)
 
         layout = QBoxLayout()
         layout.addWidget(self.num_input)
         layout.addWidget(self.layoutbtn)
         layout.addWidget(self.label)
 
+    def calculate(self):
+        res = self.num_input + self.num_input2
+        self.label.setText(f"{res}")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = QuotApp()
+    window = ParcentApp()
     window.show()
     sys.exit(app.exec())
