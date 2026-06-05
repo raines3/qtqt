ffe0ead4cca6deea90c4de76ece3c6e25e7e3e07
rainesCode <markxxxzzz98@gmail.com>
2026-06-05 18:20:12 +0900

    layout: добавлена логика генерации

---
+++ qtqt.py
@@ -0,0 +1,25 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QBoxLayout
+import sys
+
+class QuotApp (QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Генератор цитрат")
+        
+        self.resize(400, 150)
+        self.qoutes = ["Будь собой", "Сегодня все будет хорошо", "Дрежи себя в руках"]
+        self.btn = QPushButton("Показать цитату", self)
+        self.label = QLabel("Нажми на кнопку", self)
+
+        layout = QBoxLayout()
+        layout.addWidget(self.btn)
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+    
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = QuotApp()
+    window.show()
+    sys.exit(app.exec())
