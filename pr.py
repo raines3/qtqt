5f91e564c92dfb42eed7b8cb7f9f168c8e11598e
raines3 <markxxxzzz98@gmail.com>
2026-07-08 19:17:16 +0900

    h

--- pr.py
+++ pr.py
@@ -96,6 +96,8 @@ class Orion_main(QMainWindow):
                     item.setText(f'{val} т')
                 
                 self.flight_table.setItem(row, col, item)
+
+
             
 
        
@@ -132,6 +134,77 @@ class Orion_main(QMainWindow):
         self.dest_input.clear()
         self.weight_input.clear()
 
+    def change_status(self):
+        current_row = self.flight_table.currentRow()
+        if current_row == -1:
+            QMessageBox.warning(self, "Ошибка", "Выберите рейс")
+            return
+        
+        flight_id = int(self.flight_table.item(current_row, 0).text())
+        current_status = self.flight_table.item(current_row, 3).text()
+
+        new_status = "В полете" if current_status == "Формируется" else "Формируется"
+        
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+        cursor.execute("UPDATE flights SET status = ? WHERE id = ?", (new_status, flight_id))
+        conn.commit()
+        conn.close()
+    
+    def new_pilot(self):
+        pilot_name = self.pilot_name_input.text().strip()
+        if not pilot_name:
+            QMessageBox.warning(self, "Ошибка", "Введите ФИО пилота")
+            return
+        
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO pilots (name) VALUES (?)", (pilot_name,))
+        conn.commit()
+        conn.close()
+
+        self.pilot_name_input.clear()
+
+        QMessageBox.information(self, "Успешно", f"Добавлен пилот: {pilot_name}")
+
+    def on_delete(self):
+        current_row = self.flight_table.currentRow()
+        if current_row == -1:
+            QMessageBox.warning(self, "Ошибка", "Выберете рейс для удаления")
+            return
+        
+        flight_id = int(self.flight_table.item(current_row, 0).text())
+        dest = self.flight_table.item(current_row, 1).text()
+
+        conn = sqlite3.connect(db_name)
+        cursor = conn.cursor()
+        cursor.execute("SELECT pilot_id FROM flights WHERE id = ?", (flight_id,))
+        res = cursor.fetchone()
+        if res and res[0]:
+            self.cursor.execute("UPDATE pilots SET status = 'Доступно' WHERE id = ?", (flight_id,))
+
+        
+        
+        
+
+
+
+
 
 if __name__ == "__main__":
-    pass
+    
+    while True:
+        app = QApplication(sys.argv)
+        dialog = orion_dialog()
+
+        if dialog.exec() == QDialog.DialogCode.Accepted:
+            role = dialog.user_role
+            db_name = dialog.db_name
+            
+        
+            window = Orion_main()
+            window.show()
+            sys.exit(app.exec())
+
+          
+
